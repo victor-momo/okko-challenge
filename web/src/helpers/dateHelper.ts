1ce88466af3c4e04482e2ea7565d0ca9c1e8e521
Victor Morel <morel.victor.69@gmail.com>
2023-05-30 20:52:52 +0200

    feat: small helper fn to convert a string date + hour combination to a timestamp

--- web/src/helpers/dateHelper.ts
+++ web/src/helpers/dateHelper.ts
@@ -5,3 +5,19 @@ export const sameDay = (d1: Date, d2: Date) => {
     d1.getFullYear() === d2.getFullYear()
   );
 };
+
+export const dayAndHourToTimestamp = (day: Date, hour: number): number => {
+  const copyDate = new Date(
+    Date.UTC(
+      day.getFullYear(),
+      day.getMonth(),
+      day.getDate(),
+      day.getHours(),
+      day.getMinutes(),
+      day.getSeconds()
+    )
+  );
+  copyDate.setUTCHours(0, 0, 0, 0);
+
+  return copyDate.getTime() + hour * 60 * 1000 * 60;
+};
